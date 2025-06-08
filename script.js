c769b26c4adace10e647de80a7fdf1478c9d0987
anmol25e <159610433+anmol25e@users.noreply.github.com>
2025-06-08 01:38:03 -0600

    add a function to AC btn which clears everything and also to 7,8btn

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-const allClearButton = document.getElementById("allClearButton");
+const number7 = document.getElementById("seven");
+const number8 = document.getElementById("eight");
+const numberDisplay = document.getElementById("numDisplay")
+const allClearBtn = document.getElementById("allClearButton")
+let numDisplay = '';
 
-allClearButton.addEventListener("click", function() {
-    console.log("godjsf");
-});
+number7.addEventListener("click", showSeven)
+number8.addEventListener("click", showEight)
+allClearBtn.addEventListener("click", clearBtn)
+
+function clearBtn() {
+    numberDisplay.innerText = '';
+}
+
+
+function showSeven(){
+    if(numberDisplay.innerText.length < 11){
+        numberDisplay.innerText += "7"
+
+    }
+
+}
+
+function showEight(){
+    if(numberDisplay.innerText.length < 11){
+        numberDisplay.innerText += "8"
+
+    }
+
+}
+
+
