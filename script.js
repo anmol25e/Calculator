a2df99172f9c3a897b432890cdb11b904013a3f8
anmol25e <159610433+anmol25e@users.noreply.github.com>
2025-06-08 02:08:09 -0600

    made the delet btn interactive

--- script.js
+++ script.js
@@ -2,16 +2,24 @@ const number7 = document.getElementById("seven");
 const number8 = document.getElementById("eight");
 const numberDisplay = document.getElementById("numDisplay")
 const allClearBtn = document.getElementById("allClearButton")
+const deleteBtn = document.getElementById("deleteButton")
+
 let numDisplay = '';
 
 number7.addEventListener("click", showSeven)
 number8.addEventListener("click", showEight)
 allClearBtn.addEventListener("click", clearBtn)
+deleteBtn.addEventListener("click", removeBtn)
 
 function clearBtn() {
     numberDisplay.innerText = '';
 }
 
+
+function removeBtn() {
+    numberDisplay.innerText = numberDisplay.innerText.slice(0, -1);
+    
+}
 
 function showSeven(){
     if(numberDisplay.innerText.length < 11){
